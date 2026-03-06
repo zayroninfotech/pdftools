660cfd49db736519070693fbf9ef3f5d292d5a8c
saisatys4 <259488551+zayroninfotech@users.noreply.github.com>
2026-03-07 01:44:29 +0530

    Add collectstatic command to collect CSS and static files

--- start.sh
+++ start.sh
@@ -5,6 +5,10 @@ PORT=${PORT:-8000}
 echo "Using PORT: $PORT"
 echo "MONGODB_URI is set: $([ -z $MONGODB_URI ] && echo 'NO' || echo 'YES')"
 
+# Collect static files
+echo "Collecting static files..."
+python manage.py collectstatic --noinput --clear
+
 # Run gunicorn
 exec gunicorn pdftools.wsgi:application \
     --bind 0.0.0.0:$PORT \
